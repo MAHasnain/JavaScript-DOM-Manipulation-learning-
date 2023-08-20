0143a1ad99b59f7c8524bcbfc829bb67410a41f4
M A Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-08-20 22:27:53 +0500

    for prctc

---
+++ script.mjs
@@ -0,0 +1,15 @@
+///  DOM  -> document object module   ///
+
+///   4 pillars of DOM 
+// 
+//   1. selection of an element 
+//   2. Changing HTML 
+//   3. Changing css   
+//   4. Event listener
+// 
+// 
+
+
+let myName = document.querySelector('.box')
+
+console.log(myName);
