c85e23423a27d81e6b02d49f6bda32e8d013f493
M A Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-08-21 16:49:38 +0500

    ok

--- script.mjs
+++ script.mjs
@@ -10,13 +10,13 @@
 // 
 
 
-let myName = document.querySelector('.heading')
+// let myName = document.querySelector('.heading')
 
 // let a = document.querySelector('.box')
 // a.innerHTML = "as"
 
 
-myName.innerHTML = "atta"
+// myName.innerHTML = "atta"
 // myName.innerHTML = "asdf"
 
 // myName.style.backgroundColor = "blue"
@@ -30,3 +30,27 @@ myName.innerHTML = "atta"
 
 //  kisi bhi movement ko sun kr react krne wala
 
+// let heading = document.querySelector('.heading')
+// heading.addEventListener('click', function() {
+//     heading.innerHTML = "mah"
+//     heading.style.backgroundColor = "white"
+
+// })
+
+
+
+let bulb = document.querySelector('.bulb');
+let btn = document.querySelector('.btn');
+
+let clr = 0;
+
+btn.addEventListener("click", function (){
+    if (clr == 0) {
+        bulb.style.backgroundColor = "yellow"
+        clr = 1    
+    }else{
+        bulb.style.backgroundColor = "transparent"
+        clr = 0
+    }
+
+})
